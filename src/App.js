1a56174bb19c12ef095720568d9ae357941032b7
ashabilsyauqi <ashabilsyauqi@gmail.com>
2023-08-18 17:39:23 +0700

    fixed to responsive

--- src/App.js
+++ src/App.js
@@ -2,39 +2,43 @@ import { useState } from "react";
 import "./App.css";
 import { searchMovie } from "./api";
 import PopularMovieList from "./Components/popularMovieList"; // Impor komponen
+import "bootstrap/dist/css/bootstrap.min.css"; // Impor file CSS Bootstrap
 
 const App = () => {
   const [popularmovies, setPopularMovies] = useState([]);
   const [searching, setSearching] = useState(false);
-  const [searchKeyword, setSearchKeyword] = useState(""); // State untuk menyimpan keyword pencarian
+  const [searchKeyword, setSearchKeyword] = useState("");
 
   const handleSearch = async () => {
     if (searchKeyword.length > 0) {
       const query = await searchMovie(searchKeyword);
       setPopularMovies(query.results);
-      setSearching(true); // Set status searching menjadi true setelah pencarian dilakukan
+      setSearching(true);
     }
   };
 
   return (
-    <div className="App">
-      <h1 className="Title">Ashabil Movie's</h1>
-      <header className="App-header">
-       
-          <input
-            placeholder="cari film"
-            className="movie-search"
-            onChange={({ target }) => setSearchKeyword(target.value)} // Set keyword pencarian saat mengetik
-          />
-          <button className="btn" onClick={handleSearch}>
-            Search
-          </button>{" "}
-        
-        {/* Gunakan handleSearch saat tombol ditekan */}
-      </header>
-
-      <div className="movie-container">
-        {searching && <PopularMovieList movies={popularmovies} />}
+    <div className="container mt-5">
+      <h1 className="text-center mb-4">Ashabil Movie's</h1>
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search movie"
+              onChange={({ target }) => setSearchKeyword(target.value)}
+            />
+            <button className="btn btn-dark" onClick={handleSearch}>
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          {searching && <PopularMovieList movies={popularmovies} />}
+        </div>
       </div>
     </div>
   );
--- src/Components/popularMovieList.js
+++ src/Components/popularMovieList.js
@@ -1,29 +1,39 @@
 import React from "react";
-import "./Components.css";
+import "bootstrap/dist/css/bootstrap.min.css"; // Impor file CSS Bootstrap
+import './Components.css'
 
 const PopularMovieList = ({ movies }) => {
   return (
-    <>
+    <div className="d-flex justify-content-center align-items-center flex-wrap gap-5">
       {movies.length > 0 ? (
         movies.map((movie, i) => (
           <div
-            className="card"
+            className="card col-md-3 mb-4"
             key={i}
             style={{
-              backgroundImage: `url(${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path})`
+              backgroundImage: `url(${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path})`,
+              width: '350px',
+              height: '500px',
             }}
           >
-                    <div className="movie-date">{movie.release_date.split("-")[0]}</div> {/* Ambil tahun dengan split */}
-            <div className="movie-rate">
-              ⭐ : {parseFloat(movie.vote_average).toFixed(1)}
+            <div className="desc-movie">
+              <div className="movie-date">
+                {movie.release_date.split("-")[0]}
+              </div>
+              <div className="movie-rate">
+                ⭐ : {parseFloat(movie.vote_average).toFixed(1)}
+              </div>
             </div>
           </div>
         ))
       ) : (
-        <div className="no-movies-found">Film apaantu? gak nemu saya mas.. 🤷‍♂️🤷‍♀️</div>
+        <div className="no-movies-found">
+          Film apaantu? gak nemu saya mas.. 🤷‍♂️🤷‍♀️
+        </div>
       )}
-    </>
+    </div>
   );
 };
+
 
 export default PopularMovieList;
