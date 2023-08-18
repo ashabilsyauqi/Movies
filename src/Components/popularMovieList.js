31efc17c59726b8820f419a1866d5ecfea65d275
ashabilsyauqi <ashabilsyauqi@gmail.com>
2023-08-18 17:21:03 +0700

    update ui

--- src/Components/popularMovieList.js
+++ src/Components/popularMovieList.js
@@ -4,32 +4,23 @@ import "./Components.css";
 const PopularMovieList = ({ movies }) => {
   return (
     <>
-      {movies.length > 0 && (
-        <>
-          {movies.map((movie, i) => {
-            // Cek apakah ada gambar sebelum menampilkan data
-            if (movie.poster_path) {
-              return (
-                <div
-                  className="card"
-                  key={i}
-                  style={{
-                    backgroundImage: `url(${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path})`,
-                  }}
-                >
-                  <div className="desc-movie">
-                  <div className="movie-date">{movie.release_date.split("-")[0]}</div> {/* Ambil tahun dengan split */}
-                    <div className="movie-rate">
-                       ⭐:{parseFloat(movie.vote_average).toFixed(1)}
-                    </div>
-                  </div>
-                </div>
-              );
-            } else {
-              return null; // Jika tidak ada gambar, return null
-            }
-          })}
-        </>
+      {movies.length > 0 ? (
+        movies.map((movie, i) => (
+          <div
+            className="card"
+            key={i}
+            style={{
+              backgroundImage: `url(${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path})`
+            }}
+          >
+                    <div className="movie-date">{movie.release_date.split("-")[0]}</div> {/* Ambil tahun dengan split */}
+            <div className="movie-rate">
+              ⭐ : {parseFloat(movie.vote_average).toFixed(1)}
+            </div>
+          </div>
+        ))
+      ) : (
+        <div className="no-movies-found">Film apaantu? gak nemu saya mas.. 🤷‍♂️🤷‍♀️</div>
       )}
     </>
   );
